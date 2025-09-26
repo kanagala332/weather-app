1f9049f5358efbb6542ced6a829cafb61c0e6d1c
kanagala332 <Kanagalaharshavardhan@gmail.com>
2025-09-26 19:36:48 +0530

    Update WeatherAppApplication.java

--- weather-app-backend/src/main/java/com/example/weather_app/WeatherAppApplication.java
+++ weather-app-backend/src/main/java/com/example/weather_app/WeatherAppApplication.java
@@ -2,13 +2,21 @@ package com.example.weather_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WeatherAppApplication {
+public class WeatherAppApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WeatherAppApplication.class, args);
-		System.out.print("Hello world");
-	}
+    // Needed for WAR deployment to external Tomcat
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(WeatherAppApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherAppApplication.class, args);
+        System.out.print("Hello world");
+    }
 }
+
